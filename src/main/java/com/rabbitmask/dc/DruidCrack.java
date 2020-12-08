f6398033eedb54afb451457dfdaa3879c7b4ec31
RabbitMask <37649548+rabbitmask@users.noreply.github.com>
2020-12-08 20:31:38 +0800

    Update DruidCrack.java

--- src/main/java/com/rabbitmask/dc/DruidCrack.java
+++ src/main/java/com/rabbitmask/dc/DruidCrack.java
@@ -7,17 +7,28 @@ import java.util.Scanner;
 
 public class DruidCrack extends DruidPasswordCallback {
     public static void main(String[] args) {
-        System.out.print("Welcome To DruidCrack！By RabbitMask\n");
+
+        String banner = " ____          _   _ _____             _\n" +
+                "|    \\ ___ _ _|_|_| |     |___ ___ ___| |_\n" +
+                "|  |  |  _| | | | . |   --|  _| .'|  _| '_|\n" +
+                "|____/|_| |___|_|___|_____|_| |__,|___|_,_|";
+        System.out.println(banner);
+
+        System.out.print("\t\tA Tiny Tools To DruidCrack | By RabbitMask\n");
+
         Scanner s = new Scanner(System.in);
         System.out.print("请输入您的密文：");
         String pwd = s.nextLine();
-        s.close();
-        String publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKZxh/6AESER0sRSuq2K35FTNtGMv4yAvX3B/VMLx0F9VJtwhZdDgU/VEpvv1sNmRU9zp4abp+EL2dljss1VILsCAwEAAQ==";
+
+        Scanner k = new Scanner(System.in);
+        System.out.print("请输入您的公钥：");
+        String key = k.nextLine();
+
         try {
-            String decryptword = ConfigTools.decrypt(publicKey, pwd);
+            String decryptword = ConfigTools.decrypt(key, pwd);
             System.out.println("您的密码明文为："+decryptword);
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("ERROR！请确认公钥与密文是否正确");
         }
     }
 }
